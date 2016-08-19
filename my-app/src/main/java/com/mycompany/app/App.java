903950c77fd7739074325b3bc8b1ee35be2a58b3
moshen ahmed bukenya <moshenahmed@gmail.com>
2016-08-19 16:39:04 +0200

    assignement one

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,5 @@
 package com.mycompany.app;
-
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+Scanner text = new Scanner(System.in);
+  System.out.print("Enter you name :");
+    
+      String nameinput = text.nextLine();
+        for (int i = nameinput.length() - 1;  i >= 0; i--){
+	  
+	     System.out.print(nameinput.charAt(i));
+	       }
+	          
+		    
+		     }
 }
