4bd0bb82f6bce8ba27c8770b3b7bf97d405e0c02
moshen ahmed bukenya <moshenahmed@gmail.com>
2016-08-23 16:37:53 +0200

    assignmenttwo

---
+++ my-app/src/main/java/com/mycompany/app/Reverter.java
@@ -0,0 +1,13 @@
+
+public class Reverter {
+	
+	String string;
+	
+	 void Revert(){
+	
+	String reverse = new StringBuffer(string).reverse().toString();
+	System.out.println(reverse);
+	}
+	
+
+}
---
+++ my-app/src/test/java/com/mycompany/app/ReverterTest.java
@@ -0,0 +1,18 @@
+
+public class ReverterTest {
+
+	public static void main(String[] args) {
+		Reverter testone, testtwo;
+		
+		testone = new Reverter();
+		testtwo = new Reverter();
+		
+		testone.string = "moshen";
+		testone.Revert();
+		
+		testtwo.string = "navid";
+		testtwo.Revert();
+
+	}
+
+}
