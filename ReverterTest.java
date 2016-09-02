f61298383435c291b1abc7eaf31ed0a7be4580a2
moshen ahmed bukenya <moshenahmed@gmail.com>
2016-09-02 20:13:05 +0200

    working on the errors

---
+++ ReverterTest.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.app;
+
+public class ReverterTest {
+
+ public static void main(String[] args) {
+   Reverter testone, testtwo;
+
+     testone = new Reverter();
+       testtwo = new Reverter();
+
+         testone.string = "moshen";
+	   testone.Revert();
+
+	     testtwo.string = "navid";
+	       testtwo.Revert();
+
+	        }
+
+		}
